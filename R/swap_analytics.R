725dd7d1576422b226f24e842bc1e6aa83e04309
Eiichi Horigome <besuke.poripori@outlook.com>
2026-06-30 18:40:07 +0900

    Add OIS cashflow example

--- R/swap_analytics.R
+++ R/swap_analytics.R
@@ -96,3 +96,72 @@ qlg_swap_summary <- function(swap) {
     fair_spread = qlg_swap_fair_spread(swap)
   )
 }
+
+
+#' OIS overnight leg cashflow table
+#'
+#' @param swap A QuantLib OIS-like object.
+#'
+#' @export
+qlg_ois_overnight_leg_table <- function(swap) {
+  qlg_leg_to_cashflow_tbl(swap$overnightLeg())
+}
+
+#' Run OIS cashflow example
+#'
+#' @export
+qlg_ois_cashflow_example <- function() {
+  qlg_use_quantlib()
+  requireNamespace("tibble", quietly = TRUE)
+
+  qlg_eval_date("2020-10-19")
+
+  calendar <- QuantLib::TARGET()
+
+  today <- qlg_date("2020-10-19")
+
+  settlement_date <- QuantLib::Calendar_advance(
+    calendar,
+    today,
+    3,
+    "Days"
+  )
+
+  curve_input_tbl <- tibble::tibble(
+    date_chr = c(
+      "2020-10-19", "2020-11-19", "2021-01-19", "2021-04-19",
+      "2021-10-19", "2022-04-19", "2022-10-19", "2023-10-19",
+      "2025-10-19", "2030-10-19", "2035-10-19", "2040-10-19"
+    ),
+    rate = c(
+      -0.004, -0.002, 0.001, 0.005,
+      0.009, 0.010, 0.010, 0.012,
+      0.017, 0.019, 0.028, 0.032
+    )
+  )
+
+  dates <- qlg_make_date_vector(curve_input_tbl$date_chr)
+
+  forecast_curve <- QuantLib::ZeroCurve(
+    dates,
+    curve_input_tbl$rate,
+    QuantLib::Actual365Fixed()
+  )
+
+  forecast_handle <- QuantLib::YieldTermStructureHandle(forecast_curve)
+
+  swap_builder <- QuantLib::MakeOIS(
+    swapTenor = QuantLib::Period(5, "Years"),
+    overnightIndex = QuantLib::Eonia(forecast_handle),
+    fixedRate = 0.002
+  )
+
+  swap <- QuantLib::MakeOIS_makeOIS(swap_builder)
+
+  list(
+    today = QuantLib::Date_ISO(today),
+    settlement_date = QuantLib::Date_ISO(settlement_date),
+    fixed_leg = qlg_swap_fixed_leg_table(swap),
+    overnight_leg = qlg_ois_overnight_leg_table(swap)
+  )
+}
